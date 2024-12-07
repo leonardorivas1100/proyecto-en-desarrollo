f16050dde4b1ca5c9d3510111c912ad2c08039b6
Leonardo Rivas <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-07 08:27:47 +0000

    Se definieron los esquemas de validacion para las solicitudes de los roles

---
+++ proyectoX/backend/src/validators/rol_validator.js
@@ -0,0 +1,32 @@
+import Joi from 'joi';
+
+// Esquema de validación para la creación de un rol
+export const createRolSchema = Joi.object({
+  nombre: Joi.string().required().messages({
+    'string.base': 'El nombre del rol debe ser una cadena de texto.',
+    'any.required': 'El nombre del rol es un campo obligatorio.'
+  }),
+});
+
+// Esquema de validación para la actualización de un rol
+export const updateRolSchema = Joi.object({
+  nombre: Joi.string().optional().messages({
+    'string.base': 'El nombre del rol debe ser una cadena de texto.',
+  }),
+});
+
+// Esquema de validación para obtener un rol especiificado por su Id
+export const getRolSchema = Joi.object({
+  id: Joi.string().required().messages({
+    'string.base': 'El ID del rol debe ser una cadena de texto.',
+    'any.required': 'El ID del rol es obligatorio.'
+  }),
+});
+
+// Esquema de validación para eliminar un rol por su número de identificación
+export const deleteRolSchema = Joi.object({
+  id: Joi.string().required().messages({
+    'string.base': 'El ID del rol debe ser una cadena de texto.',
+    'any.required': 'El ID del rol es obligatorio.'
+  }),
+});
