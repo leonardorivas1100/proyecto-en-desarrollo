cb4f99358e6f96533c032aa42b1f2a279df1ef59
Leonardo Rivas <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-10 07:06:16 +0000

    Importe rutas de usuario y autenticacion

--- proyectoX/backend/src/index.js
+++ proyectoX/backend/src/index.js
@@ -6,15 +6,13 @@ import {port} from './config/connect.js'; // Puerto de donde corre el servidor
 import "./config/connect.js" // Conexion con las base
 import { swaggerJSDocs } from './swagger.js'; // Documentación Swagger
 import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
+
+import authRoutes from './routes/auth_routes.js'
 import rolRoutes from './routes/rol_routes.js';
+import userRoutes from './routes/usuarios_routes.js';
 
 // Configuración de las variables de entorno
 dotenv.config();
-
-// Importamos las rutas 
-import rolRoutes from './routes/rol_routes.js';
-
 
 // Se crea la instancia que manejará los aspectos del servidor
 const app = express();
@@ -26,10 +24,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // Añadimos las rutas a la aplicación
-app.use('/api/roles', rolRoutes);
-
-// Usamos la ruta de autenticacion
 app.use('/api/auth', authRoutes);
+app.use('/api/roles', rolRoutes);
+app.use('/api/usuarios', userRoutes);
+
 
 // Configurar Swagger para la documentación de la API
 swaggerJSDocs(app, port);
