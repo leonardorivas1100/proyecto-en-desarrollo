e09210cde6721117b3f5600f7bc457d5a76e0851
Leonardo Rivas <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-10 04:08:07 +0000

    Dependencias generales y dependencias de desarrollo instaladas para la implementacion del Jwt

--- proyectoX/backend/src/index.js
+++ proyectoX/backend/src/index.js
@@ -1,10 +1,16 @@
 // Importamos las dependencias necesarias para que el servidor funcione
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import {port} from './config/connect.js'; // Puerto de donde corre el servidor
 import "./config/connect.js" // Conexion con las base
 import { swaggerJSDocs } from './swagger.js'; // Documentación Swagger
+import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
+import rolRoutes from './routes/rol_routes.js';
 
+// Configuración de variables de entorno
+dotenv.config();
 
 // Importamos las rutas 
 import rolRoutes from './routes/rol_routes.js';
@@ -13,19 +19,17 @@ import rolRoutes from './routes/rol_routes.js';
 // Se crea la instancia que manejará los aspectos del servidor
 const app = express();
 
-// Definimos las opciones que tendrá el dominio especificado
-const corsOptions = {
-    origin: 'http//localhost:10000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    optionsSuccessStatus: 200,
-};
 
 // Permitimos que el servidor pueda leer solicitudes de cuerpi JSON, como PUT O POST.
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 // Añadimos las rutas a la aplicación
 app.use('/api/roles', rolRoutes);
+
+// Usamos la ruta de autenticacion
+app.use('/api/auth', authRoutes);
 
 // Configurar Swagger para la documentación de la API
 swaggerJSDocs(app, port);
