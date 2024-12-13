abc2a3dfd5fb620b807b00fe6a367f3e58190dc6
Leonardo Rivas <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-13 06:14:00 +0000

    Hice el controlador para modificar un usuario por su numero de indentificacion

--- proyectoX/backend/src/controllers/usuarios_controller.js
+++ proyectoX/backend/src/controllers/usuarios_controller.js
@@ -55,11 +55,14 @@ export const create_user = async (req, res) => {
     }
 };
 
+
 // 2. Obtener todos los Usuarios.
 export const see_all_users = async (req, res) => {
     try {
-        const users = await User.find(); // Busca todos los usuarios
-        res.status(200).json(users);
+        const user = await Usuario
+        .find() // Busca todos los usuarios
+        .populate('id_rol') // Busca y llena el campo id_rol con el nombre del rol correspondiente
+        res.status(200).json(user);
     }
     catch (error) {
         console.error(error);
@@ -69,4 +72,78 @@ export const see_all_users = async (req, res) => {
         });
     }
 };
+
+
+// 3. Actualizar un usuario por su Id.
+export const update_user = async (req, res) => {
+    try {
+            // Verificar si el usuario existe
+            const { numeroIdentificacion } = req.params;
+            const { nombres, apellidos, telefono, email, password, id_rol  } = req.body;
+            const user = await Usuario.findOne({numeroIdentificacion});
+    
+            // En caso de no haya un usuario para actualizar
+            if (!user) {
+                return res.status(404).json({
+                    Usuario_no_encontrado: 'No pudo encontrarse este usuario.'
+                });
+            }
+    
+            // Verificar si el nuevo nombre ya está en uso por otro usuario
+            const name_user_now = await Usuario.findOne({ nombres, apellidos });
+            if (name_user_now && name_user_now.numeroIdentificacion !== numeroIdentificacion) {
+                return res.status(400).json({
+                    Error_nombre: 'Ya existe un usurario con este nombre'
+                });
+            }
+
+            // Verificar si la nueva identificacion ya está en uso por otro usuario
+            const identification_user_now = await Usuario.findOne({numeroIdentificacion });
+            if (identification_user_now && identification_user_now.numeroIdentificacion !== numeroIdentificacion) {
+                return res.status(400).json({
+                    Error_identificacion: 'Ya existe un usurario con este numero de identificacion'
+                });
+            }
+
+            // Verificar el nuevo correo ya está en uso por otro usuario
+            const email_user_now = await Usuario.findOne({ email });
+            if (email_user_now && email_user_now.numeroIdentificacion !== numeroIdentificacion) {
+                return res.status(400).json({
+                    Error_email: 'Ya existe un usurario con este correo electronico'
+                });
+            }
+
+            // Verificar el numero telefonico ya está en uso por otro usuario
+            const phone_user_now = await Usuario.findOne({ telefono });
+            if (phone_user_now && phone_user_now.numeroIdentificacion !== numeroIdentificacion) {
+                return res.status(400).json({
+                    Error_number: 'Ya existe un usurario con este numero telfonico'
+                });
+            }
+
+            // Actualizar el usuario.
+        user.nombres = nombres;
+        user.apellidos = apellidos;
+        user.telefono = telefono;
+        user.email = email;
+        user.password = password;
+        user.id_rol = id_rol;
+
+
+        const usuario_updated = await user.save();
+
+        res.status(200).json({
+            Completado: 'User actualizado correctamente',
+            usuario_actualizado: usuario_updated,
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            Error: 'Error al actualizar el usuario',
+            error: error.message,
+        });
+    }
+};
+
 
