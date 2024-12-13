c315945cdf441b97a4e05c84e7db4bcb79faf0b9
Leonardo Rivas <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-13 04:54:45 +0000

    Agregue la ruta de mirar todos los usuarios al archivo de rutas

--- proyectoX/backend/src/routes/usuarios_routes.js
+++ proyectoX/backend/src/routes/usuarios_routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { create_user } from '../controllers/usuarios_controller.js';
+import { create_user, see_all_users } from '../controllers/usuarios_controller.js';
 
 const router = express.Router();
 
 // Ruta para crear un nuevo usuario
 router.post('/', create_user);
+
+// Ruta para crear un nuevo usuario
+router.get('/', see_all_users);
+
 
 export default router;
