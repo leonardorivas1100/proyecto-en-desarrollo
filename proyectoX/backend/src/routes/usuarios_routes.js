b1d41a07538f1d912a0620c265a21dc0ae610593
Leonardo Rivas <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-13 06:15:43 +0000

    Agregue la ruta para actualizar un usuario

--- proyectoX/backend/src/routes/usuarios_routes.js
+++ proyectoX/backend/src/routes/usuarios_routes.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import { create_user, see_all_users } from '../controllers/usuarios_controller.js';
+import { create_user, see_all_users, update_user } from '../controllers/usuarios_controller.js';
 
 const router = express.Router();
 
-// Ruta para crear un nuevo usuario
+// 1. Ruta para crear un nuevo usuario.
 router.post('/', create_user);
 
-// Ruta para crear un nuevo usuario
+// 2. Ruta para obtener todos los usuarios.
 router.get('/', see_all_users);
+
+// 3. Ruta para editar un usuario por su Id.
+router.put('/:numeroIdentificacion', update_user);
 
 
 export default router;
