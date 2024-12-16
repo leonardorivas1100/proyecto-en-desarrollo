904404211e22b3b5f8c847c500aa1d5cd0495404
leonardorivas1100 <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-16 01:44:23 -0500

    Termine las modificaciones necesarias para la implementacion de la documentacion correcta

--- proyectoX/backend/src/routes/usuarios_routes.js
+++ proyectoX/backend/src/routes/usuarios_routes.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import { validatorHandler } from '../middleware/validator.handler.js';
-import { createUserSchema, updateUserSchema, getUserSchema,  } from '../validators/usuario_validator.js';
+import { createUserSchema, updateUserSchema } from '../validators/usuario_validator.js';
 import { create_user, see_all_users, update_user, found_user, delete_user } from '../controllers/usuarios_controller.js';
-// , , , deleteUserSchema
+
 const router = express.Router();
 
-// 1. Ruta para crear un nuevo usuario.
 /**
  * @swagger
  * tags:
@@ -16,13 +15,13 @@ const router = express.Router();
  * @swagger
  * components:
  *   schemas:
- *     usuario:
+ *     Usuario:
  *       type: object
  *       properties:
  *         numeroIdentificacion:
  *           type: string
  *           description: Numero de identificacion del usuario.
- *         nombres:
+ *         nombres: 
  *           type: string
  *           descripcion: Nombres del usuario.
  *         apellidos:
@@ -49,6 +48,9 @@ const router = express.Router();
  *         - password 
  *         - nombre_rol 
  */
+
+
+// 1. Ruta para crear un nuevo usuario.
 /**
  * @swagger
  * /api/usuarios:
@@ -71,17 +73,122 @@ const router = express.Router();
  */
 router.post('/', validatorHandler(createUserSchema, "body"), create_user);
 
+
 // 2. Ruta para obtener todos los usuarios.
+/**
+ * @swagger
+ * /api/usuarios:
+ *   get:
+ *     summary: Mirar todos los usuarios registrados
+ *     tags: [Usuario]
+ *     responses:
+ *       201:
+ *         description: Lista de usuarios
+ *       500:
+ *         description: Error al obtener los usuarios
+ */
 router.get('/', see_all_users);
 
-// 3. Ruta para editar un usuario por su numero de identificacion.
-router.put('/:numeroIdentificacion', validatorHandler(updateUserSchema, "params"), update_user);
 
-// 4. Ruta para buscar un usuario por su numero de identificacion.
-router.get('/:numeroIdentificacion', validatorHandler(getUserSchema, "body"), found_user);
+// 3. Ruta para buscar un usuario por su numero de identificacion.
+/**
+ * @swagger
+ * /api/usuarios/{numeroIdentificacion}:
+ *   get:
+ *     summary: Obtener un usuario especificado por su numero de identificacion
+ *     description: Obtendrá un usuario especificado por su nombre
+ *     tags: [Usuario]
+ *     parameters:
+ *       - name: numeroIdentificacion
+ *         in: path
+ *         required: true
+ *         description: Numero de identificacion del usuario
+ *     responses:
+ *       200:
+ *         description: Usuario no encontrado
+ *       404:
+ *         description: Usuario no encontrado
+ *       500:
+ *         description: Error al obtener el usuario
+ */
+router.get('/:numeroIdentificacion', found_user);
+
+
+// 4. Ruta para editar un usuario por su numero de identificacion.
+/**
+ * @swagger
+ * /api/usuarios/{numeroIdentificacion}:
+ *   put:
+ *     summary: Actualizar un usuario por número de identificación
+ *     description: Actualiza los detalles de un usuario existente mediante su número de identificación.
+ *     tags: [Usuario]
+ *     parameters:
+ *       - name: numeroIdentificacion
+ *         in: path
+ *         required: true
+ *         description: Número de identificación del usuario
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nuevoNumeroIdentificacion:
+ *                 type: string        
+ *               nombres:
+ *                 type: string
+ *               apellidos:
+ *                 type: string
+ *               telefono:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               nombre_rol:
+ *                 type: string
+ *             required:
+ *               - nuevoNumeroIdentificacion   
+ *               - nombres
+ *               - apellidos
+ *               - telefono
+ *               - email
+ *               - password
+ *               - nombre_rol
+ *     responses:
+ *       200:
+ *         description: Usuario actualizado correctamente
+ *       404:
+ *         description: Usuario no encontrado
+ *       500:
+ *         description: Error interno en el servidor
+ */
+router.put('/:numeroIdentificacion', validatorHandler(updateUserSchema, "body"), update_user);
+
 
 // 5. Ruta para eliminar un usuario por su numero de identificacion.
-router.delete('/:numeroIdentificacion', delete_user);
+/**
+ * @swagger
+ * /api/usuarios/{numeroIdentificacion}:
+ *   delete:
+ *     summary: Eliminar un usuario por número de identificación
+ *     description: Elimina un usuario de la base de datos utilizando su número de identificación.
+ *     tags: [Usuario]
+ *     parameters:
+ *       - name: numeroIdentificacion
+ *         in: path
+ *         required: true
+ *         description: Número de identificación del usuario
+ *     responses:
+ *       200:
+ *         description: Usuario eliminado correctamente
+ *       404:
+ *         description: Usuario no encontrado
+ *       500:
+ *         description: Error interno en el servidor
+ */
+router.delete('/:numeroIdentificacion',  delete_user);
 
 
 export default router;
