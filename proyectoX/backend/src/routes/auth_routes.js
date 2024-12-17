6f215e6ba40b56c56b0ea4c68063f96c81354d51
leonardorivas1100 <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-17 17:45:19 -0500

    Agregar doucmentacion a mi api de autenticacion

--- proyectoX/backend/src/routes/auth_routes.js
+++ proyectoX/backend/src/routes/auth_routes.js
@@ -3,7 +3,54 @@ import { login } from '../controllers/autenticacion_controller.js'; // Importar 
 
 const router = express.Router();
 
-// Ruta para el inicio de sesión
+// Definimos las rutas para los roles.
+const RolRouter = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Au
+ *   description: Autentication enveloped.
+ */
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Au:
+ *       type: object
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: Correo electronico del usuario.
+ *         password:
+ *           type: string
+ *           description: Contraseña.
+ *       required:
+ *         - nombre
+ *         - password 
+ */
+
+// A. Ruta para el inicio de sesión
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Autenticar un usuario
+ *     tags: [Au]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Au'
+ *     responses:
+ *       201:
+ *         description: Rol created successfully
+ *       400:
+ *         description: El Rol ya existe
+ *       500:
+ *         description: Error al crear el rol
+ */
 router.post('/login', login);
 
 export default router;
