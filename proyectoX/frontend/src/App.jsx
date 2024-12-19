aa514ca207e29941aaaf96607eada9ea018c560a
leonardorivas1100 <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-19 02:15:41 -0500

    Cambios en los archivos del frontend para su correcto funcionamiento, ademas de cambios para las pruebas de inicio de sesion

--- proyectoX/frontend/src/App.jsx
+++ proyectoX/frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
