93312b2f04e3cbb016b5679cf6877c9196470aa0
leonardorivas1100 <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-22 21:22:06 -0500

    Modificar unas cositas en el front para usar la carpeta de contextos

--- proyectoX/frontend/src/App.jsx
+++ proyectoX/frontend/src/App.jsx
@@ -1,49 +1,50 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext'; // Importa el AuthProvider
 import LoginPage from './pages/login-page';
 import DashboardAdmin from './pages/dashboards/dashboard-administrador';
 import DashboardAsistente from './pages/dashboards/dashboard-asistente';
 import DashboardCliente from './pages/dashboards/dashboard-cliente';
 import DashboardNo from './pages/dashboards/dashboard-denied-access'; // Ruta correcta del archivo
-import PrivateRoute from './components/protect-route/ProtectRoute'; // Importa el componente
-
+import PrivateRoute from './components/protect-route/ProtectRoute'; // Importa el componente de rutas protegidas
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Ruta para el login */}
-        <Route path="/" element={<LoginPage />} />
+      <AuthProvider>
+        <Routes>
+          {/* Ruta para el login */}
+          <Route path="/" element={<LoginPage />} />
 
-        {/* Rutas protegidas por rol */}
-        <Route 
-          path="/dashboard/administrador" 
-          element={
-            <PrivateRoute requiredRole="administrador">
-              <DashboardAdmin />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/dashboard/asistente" 
-          element={
-            <PrivateRoute requiredRole="asistente">
-              <DashboardAsistente />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/dashboard/cliente" 
-          element={
-            <PrivateRoute requiredRole="cliente">
-              <DashboardCliente />
-            </PrivateRoute>
-          } 
-        />
-        
-        <Route path="/dashboard/no-permiso" element={<DashboardNo />} />
-
-      </Routes>
+          {/* Rutas protegidas por rol */}
+          <Route 
+            path="/dashboard/administrador" 
+            element={
+              <PrivateRoute requiredRole="administrador">
+                <DashboardAdmin />
+              </PrivateRoute>
+            } 
+          />
+          <Route 
+            path="/dashboard/asistente" 
+            element={
+              <PrivateRoute requiredRole="asistente">
+                <DashboardAsistente />
+              </PrivateRoute>
+            } 
+          />
+          <Route 
+            path="/dashboard/cliente" 
+            element={
+              <PrivateRoute requiredRole="cliente">
+                <DashboardCliente />
+              </PrivateRoute>
+            } 
+          />
+          
+          <Route path="/dashboard/no-permiso" element={<DashboardNo />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
--- proyectoX/frontend/src/components/login-form/useLoginForm.js
+++ proyectoX/frontend/src/components/login-form/useLoginForm.js
@@ -1,60 +1,21 @@
 // components/login-form/useLoginForm.js
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
 
 const useLoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const navigate = useNavigate();
-  const URL = 'http://localhost:10000/api/auth/login';
+  const { login } = useAuth();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError(''); // Resetea el error antes de intentar iniciar sesión
+    setError('');
 
     try {
-      const response = await fetch(URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-
-      const data = await response.json();
-
-      if (response.ok) {
-        const { Token_generated, User_rol } = data;
-
-        if (!Token_generated || !User_rol) {
-          setError('Respuesta inesperada del servidor.');
-          return;
-        }
-
-        // Guarda el token y el rol en localStorage
-        localStorage.setItem('token', Token_generated);
-        localStorage.setItem('userRole', User_rol);
-
-        // Redirige al dashboard según el rol
-        switch (User_rol) {
-          case 'administrador':
-            navigate('/dashboard/administrador');
-            break;
-          case 'asistente':
-            navigate('/dashboard/asistente');
-            break;
-          case 'cliente':
-            navigate('/dashboard/cliente');
-            break;
-          default:
-            setError('Rol de usuario no reconocido.');
-        }
-      } else {
-        setError(data.Request_failed || 'Error desconocido');
-      }
+      await login(email, password);
     } catch (err) {
-      setError('Error de conexión. Por favor, inténtalo más tarde.');
+      setError(err.message || 'Error desconocido.');
     }
   };
 
