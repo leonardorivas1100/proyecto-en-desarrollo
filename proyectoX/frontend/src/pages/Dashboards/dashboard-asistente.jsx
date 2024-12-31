2dcf6e8ed14d5305d727c4934d293c159da04412
leonardorivas1100 <168871358+leonardorivas1100@users.noreply.github.com>
2024-12-30 20:04:53 -0500

    Modificaciones para implementar la separacion por carpetas

--- proyectoX/frontend/src/pages/Dashboards/dashboard-asistente.jsx
+++ proyectoX/frontend/src/pages/Dashboards/dashboard-asistente.jsx
@@ -1,24 +1,36 @@
 // DashboardAdmin.jsx
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from "../../components/navbar/Navbar"
+import UserForm from "../../components/use-form/UseForm";
+import { createUser } from "../../services/api";
 
 const DashboardAsistente = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleCreateUser = async (formData) => {
+    try {
+      await createUser(formData);
+      alert("Usuario creado exitosamente");
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Hubo un problema al crear el usuario");
+    } finally {
+      setShowForm(false);
+    }
+  };
+
   return (
     <div>
       <Navbar />
-      <div style={styles.content}>
+      <div style={{ marginTop: '70px', padding: '20px' }}>
         <h1>Bienvenido al Dashboard del Asistente</h1>
-        {/* Contenido específico del Dashboard */}
+        <button onClick={() => setShowForm(true)} className="button-primary">
+          Crear un usuario
+        </button>
+        {showForm && <UserForm onClose={() => setShowForm(false)} onSubmit={handleCreateUser} />}
       </div>
     </div>
   );
-};
-
-const styles = {
-  content: {
-    marginTop: '70px', // Espacio igual o mayor al alto del navbar
-    padding: '20px',
-  },
 };
 
 export default DashboardAsistente;
